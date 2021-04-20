7fdbadf2006294904916bbe92001e746847a99e3
gunjan-191 <53703218+gunjan-191@users.noreply.github.com>
2021-04-20 15:47:46 +0530

    Scrapping using Selenium

---
+++ Harvay.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+
+
+def Harvey(url):
+	
+	driver = webdriver.Chrome(executable_path='E://gunjan//chromedriver.exe')
+	driver.get(url)
+
+	driver.maximize_window()
+
+	title = driver.find_element_by_xpath('//*[@id="overview"]/div[1]/h1/span[1]').text
+	print(title)
+
+
+	#click the review button
+	driver.find_element_by_xpath('//*[@id="tab-product-reviews"]').click()
+
+
+	driver.implicitly_wait(10) # seconds
+
+	#click Show all
+	driver.find_element_by_xpath('//*[@id="BVRRContainer"]/div/div/div/div/div[1]/div[3]/div/div[2]/div[2]/div/div/div[1]/div/div[2]/div/div/div/p/a').click()
+
+	review = driver.find_element_by_xpath('//*[@id="bv-mbox-lightbox-list"]/div[2]/div/div/div/ol/div/li/div[2]/div[1]/div/div[1]/div/div[2]/h3').text
+	print(review)
+
+	driver.close()
+
+url = 'https://www.harveynorman.com.au/hp-14-inch-celeron-n4020-4gb-64gb-emmc-laptop.html'
+
+
+Harvey(url)
